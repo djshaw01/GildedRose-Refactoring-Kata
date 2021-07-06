e797af39ec894a63abe376262a49447c6b976d52
Daniel Shaw <daniel_shaw@shi.com>
2021-07-06 16:22:52 -0400

    Dont touch the Items property

--- csharpcore/Program.cs
+++ csharpcore/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace csharpcore
 {
@@ -10,36 +11,40 @@ namespace csharpcore
             Console.WriteLine("OMGHAI!");
             var betterBuilder = new ItemDecoratorBuilder();
 
-            IList<ItemDecorator> Items = new List<ItemDecorator>{
-                betterBuilder.build(
-                    new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20}),
-                betterBuilder.build(new Item {Name = "Aged Brie", SellIn = 2, Quality = 0}),
-                betterBuilder.build(new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7}),
-                betterBuilder.build(new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80}),
-                betterBuilder.build(new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80}),
-                betterBuilder.build(new Item
+            IList<Item> Items = new List<Item>
+            {
+                new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+                new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80},
+                new Item
                 {
                     Name = "Backstage passes to a TAFKAL80ETC concert",
                     SellIn = 15,
                     Quality = 20
-                }),
-                betterBuilder.build(new Item
+                },
+                new Item
                 {
                     Name = "Backstage passes to a TAFKAL80ETC concert",
                     SellIn = 10,
                     Quality = 49
-                }),
-                betterBuilder.build(new Item
+                },
+                new Item
                 {
                     Name = "Backstage passes to a TAFKAL80ETC concert",
                     SellIn = 5,
                     Quality = 49
-                }),
-				// this conjured item does not work properly yet
-                betterBuilder.build(new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6})
+                },
+                // this conjured item does not work properly yet
+                new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
             };
 
-            var app = new GildedRose(Items);
+
+            IList<ItemDecorator> diItems = new List<ItemDecorator>(); 
+                Items.Select(x => x).ToList().ForEach(x => { diItems.Add(betterBuilder.build(x));});
+
+            var app = new GildedRose(diItems);
 
 
             for (var i = 0; i < 31; i++)
